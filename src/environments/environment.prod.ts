f6669517b08b68f4dfc3817382f1911f05f39d44
ryzhak <ryzhak.vladimir@gmail.com>
2018-10-18 19:52:39 +0300

    updated kovan addresses

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -16,11 +16,11 @@ export const environment = {
 			daoBaseAddress: ""
 		},
 		kovan: {
-			devZenDaoAddress: "",
-			devZenDaoAutoAddress: "",
-			devZenTokenAddress: "",
-			devZenRepTokenAddress: "",
-			daoBaseAddress: ""
+			devZenDaoAddress: "0x08544530b359730100cafe3f62b8906584adf8a5",
+			devZenDaoAutoAddress: "0xca74e98be84c5f76f41d9369af2e2ba65221a0b0",
+			devZenTokenAddress: "0x76299b1bcc36b0aad1ffcd52315a1396823b59a3",
+			devZenRepTokenAddress: "0xa872d4d6e8c20d3579a8f2c4727a3454460ab622",
+			daoBaseAddress: "0x0b6e6165d1de9d806672d6b240f3004d4fd24f8a"
 		},
 		rinkeby: {
 			devZenDaoAddress: "",
